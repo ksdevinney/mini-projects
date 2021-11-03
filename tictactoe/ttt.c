dbbe2dbaf20c18d338cd0ee49b30591a57e910b1
ksdevinney <katie@devinney.org>
2021-11-03 17:00:39 -0500

    variables and initial for loop

--- tictactoe/ttt.c
+++ tictactoe/ttt.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    
+    int winner = 0;
+    int count = 0;
+
+    int positions[9];
+    int index;
+    int sign;
+    int player;
+    int flag;
+    int i, j, k;
+
+    for (i = 0; i < 9; ++)
+    {
+        
+    }
 }
