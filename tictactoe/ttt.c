9d1402c311a5d2d99778054be71ff869000d57ff
ksdevinney <katie@devinney.org>
2021-11-08 19:03:03 -0600

    for and while

--- tictactoe/ttt.c
+++ tictactoe/ttt.c
@@ -13,6 +13,18 @@ int main() {
 
     for (i = 0; i < 9; ++)
     {
-        
+        positions[i] = ' ';
+    }
+
+    while(count < 9 && winner != 1)
+    {
+        flag = 0;
+        // print the board
+        printf("\n");
+        printf("%c|%c|%c", positions[0], positions[1], positions[2]);
+        printf("-+-+-");
+        printf("%c|%c|%c", positions[3], positions[4], positions[5]);
+        printf("-+-+-");
+        printf("%c|%c|%c", positions[6], positions[7], positions[8]);
     }
 }
