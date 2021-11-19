764ca926f49c50c2ec63c9211a8edb1e06919b3d
ksdevinney <katie@devinney.org>
2021-11-19 15:24:41 -0600

    start functions

---
+++ flappy-bird/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("keypress", handleStart, { once: true });
+const title = document.querySelector("[data-title]");
+
+let lastTime;
+
+updateLoop = (time) => {
+    if (lastTime == null) {
+        lastTime = time;
+        window.requestAnimationFrame(updateLoop);
+        return;
+    }
+
+    window.requestAnimationFrame(updateLoop);
+    lastTime = time;
+
+    console.log(time - lastTime);
+}
+
+// start game
+handleStart = () => {
+    title.classList.add("hide");
+    window.requestAnimationFrame(updateLoop);
+};
+
+// end game
+handleLose = () => {
+
+}
