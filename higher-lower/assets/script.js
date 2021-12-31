95d02bc4083593c715be3fcdbc2b909f41a33ac6
ksdevinney <katie@devinney.org>
2021-12-30 20:38:16 -0600

    start functions

--- higher-lower/assets/script.js
+++ higher-lower/assets/script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+let firstNumber = 0;
+let targetNumber = 0;
+let userGuess = 0;
+let win = false;
+
+let higher;
+
+const higherButton = document.getElementById("guessHigher");
+const lowerButton = document.getElementById("guessLower");
+
+function guessNumbers() {
+    firstNumber = Math.floor(Math.random() * 11);
+    targetNumber = Math.floor(Math.random() * 11);
+    console.log(firstNumber, targetNumber);
+}
+
+function buttonRespond() {
+    console.log("hello");
+}
+
+guessNumbers();
+higherButton.addEventListener("click", buttonRespond);
+lowerButton.addEventListener("click", buttonRespond);
