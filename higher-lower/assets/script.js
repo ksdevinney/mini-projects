948f285323c6d5e63a4200e99b2565d585a68ac8
ksdevinney <katie@devinney.org>
2021-12-30 20:46:09 -0600

    higher variable

--- higher-lower/assets/script.js
+++ higher-lower/assets/script.js
@@ -3,15 +3,27 @@ let targetNumber = 0;
 let userGuess = 0;
 let win = false;
 
+// for correct answer
 let higher;
 
+// button elements
 const higherButton = document.getElementById("guessHigher");
 const lowerButton = document.getElementById("guessLower");
 
+// pick two numbers
 function guessNumbers() {
     firstNumber = Math.floor(Math.random() * 11);
     targetNumber = Math.floor(Math.random() * 11);
     console.log(firstNumber, targetNumber);
+
+    if (firstNumber > targetNumber) {
+        higher = false;
+    } else if (firstNumber < targetNumber) {
+        higher = true;
+    } else {
+        // if they are equal, roll again
+        guessNumbers();
+    }
 }
 
 function buttonRespond() {
