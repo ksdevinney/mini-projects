867e06ccf26b946eeacd3dbc05c04be24bf3ff42
ksdevinney <katie@devinney.org>
2022-01-16 21:19:13 -0600

    the pipes

--- flappy-bird/pipe.js
+++ flappy-bird/pipe.js
@@ -1,21 +1,51 @@
 const HOLE_HEIGHT = 120;
 
 function createPipe() {
-    const pipeElement = document.createElement("div");
-    const topElement = createPipeSegment("top");
-    const bottomElement = createPipeSegment("bottom");
-    pipeElement.append(topElement);
-    pipeElement.append(bottomElement);
-    pipeElement.classList.add("pipe");
-    pipeElement.style.setProperty("--hole-top", randomNumberBetween(HOLE_HEIGHT * 1.5, window.innerHeight - HOLE_HEIGHT * 0.5));
+  const pipeElement = document.createElement("div");
+  const topElement = createPipeSegment("top");
+  const bottomElement = createPipeSegment("bottom");
+  pipeElement.append(topElement);
+  pipeElement.append(bottomElement);
+  pipeElement.classList.add("pipe");
+  pipeElement.style.setProperty(
+    "--hole-top",
+    randomNumberBetween(
+      HOLE_HEIGHT * 1.5,
+      window.innerHeight - HOLE_HEIGHT * 0.5
+    )
+  );
+
+  const pipe = {
+    get left() {
+      return parseFloat(
+        getComputedStyle(pipeElem).getPropertyValue("--pipe-left")
+      );
+    },
+    set left(value) {
+      pipeElem.style.setProperty("--pipe-left", value);
+    },
+    remove() {
+      pipes = pipes.filter((p) => p !== pipe);
+      pipeElem.remove();
+    },
+    rects() {
+      return [
+        topElem.getBoundingClientRect(),
+        bottomElem.getBoundingClientRect(),
+      ];
+    },
+  };
+  pipe.left = window.innerWidth;
+  document.body.append(pipeElem);
+  pipes.push(pipe);
 }
 
 function createPipeSegment(position) {
-    const segment = document.createElement("div");
-    segment.classList.add("segment", position);
-    return segment;
+  const segment = document.createElement("div");
+  segment.classList.add("segment", position);
+  return segment;
 }
 
 function randomNumberBetween(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
